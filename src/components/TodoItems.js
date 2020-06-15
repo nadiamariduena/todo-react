75460bb1be650a2a1ecc2ad6d15d5ea2f75f9e8b
melissa <nadia.mm.mariduena@gmail.com>
2020-06-15 08:25:09 +0200

    more info

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -4,6 +4,14 @@ class TodoItems extends Component {
   createTasks(item) {
     //   the value of the task
     return <li key={item.key}>{item.text}</li>;
+    // The Key is a string attribute that identifies items in lists of elements.
+    // the Key help REACT indentify which items have changed / added or removed.
+    //the Key give the element an stable identity
+    // its not recommendable to use index is the order of the ITEM might change.
+    // it can cause performace  or issues with the components state
+    // IF YOU CHOOSE not to assign an explicit key to the items, react by default will assign INDEX
+
+    // https://www.youtube.com/watch?v=Jh47pOXwGq0
   }
 
   render() {
