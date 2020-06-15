432bfbf91246251c49745661ad14fcddb79bbb83
melissa <nadia.mm.mariduena@gmail.com>
2020-06-15 08:13:19 +0200

    Updates

---
+++ precache-manifest.513d4d37ea32c576493e88cf7b02d1b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e315f0953a60fbb699c37a852e2fc793",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "70e762d94ba82ae7c7c9",
+    "url": "/todo-react/static/css/main.ca6fdf64.chunk.css"
+  },
+  {
+    "revision": "3330d95aa1b946612365",
+    "url": "/todo-react/static/js/2.3809ba7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.3809ba7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e762d94ba82ae7c7c9",
+    "url": "/todo-react/static/js/main.53306682.chunk.js"
+  },
+  {
+    "revision": "2a39e3e74947b445e36b",
+    "url": "/todo-react/static/js/runtime-main.2ec7ebeb.js"
+  }
+]);
